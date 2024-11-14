2023502abdc1d27f5294d31815f56e0b6096536e
Alex Rodríguez Navas <alex@latitude.so>
2024-11-14 14:45:57 +0100

    wip

--- apps/gateway/src/routes/api/v2/projects/[projectId]/versions/[versionUuid]/documents/index.ts
+++ apps/gateway/src/routes/api/v2/projects/[projectId]/versions/[versionUuid]/documents/index.ts
@@ -1,13 +1,13 @@
+import { OpenAPIHono } from '@hono/zod-openapi'
 import { getHandler } from '$/routes/api/v1/projects/[projectId]/versions/[versionUuid]/documents/handlers/get'
-import { Hono } from 'hono'
 
 import { runHandler } from './handlers/run'
 import { logsRouterV2 } from './logs'
 
-const router = new Hono()
+const router = new OpenAPIHono()
 
 router.get('/:documentPath{.+}', ...getHandler)
-router.post('/run', ...runHandler)
+router.route('/run', runHandler)
 router.route('/logs', logsRouterV2)
 
 export { router as documentsRouter }
--- apps/gateway/src/routes/app.ts
+++ apps/gateway/src/routes/app.ts
@@ -1,7 +1,7 @@
+import { OpenAPIHono } from '@hono/zod-openapi'
 import authMiddleware from '$/middlewares/auth'
 import errorHandlerMiddleware from '$/middlewares/errorHandler'
 import rateLimitMiddleware from '$/middlewares/rateLimit'
-import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 
 import { chatsRouter as conversationsRouterV1 } from './api/v1/conversations/[conversationUuid]'
@@ -9,7 +9,7 @@ import { documentsRouter as documentsRouterV1 } from './api/v1/projects/[project
 import { conversationsRouter as conversationsRouterV2 } from './api/v2/conversations/[conversationUuid]'
 import { documentsRouter as documentsRouterV2 } from './api/v2/projects/[projectId]/versions/[versionUuid]/documents'
 
-const app = new Hono()
+const app = new OpenAPIHono()
 
 // Middlewares
 if (process.env.NODE_ENV !== 'test') {
@@ -19,6 +19,38 @@ if (process.env.NODE_ENV !== 'test') {
 app.get('/health', (c) => {
   return c.json({ status: 'ok' })
 })
+
+app
+  .doc('/doc', (c) => ({
+    openapi: '3.0.0',
+    info: {
+      version: '1.0.0',
+      title: 'Latitude',
+    },
+    externalDocs: {
+      url: 'https://docs.latitude.so',
+      description: 'Latitude Documentation',
+    },
+    servers: [
+      {
+        url: new URL(c.req.url).origin,
+        description: 'Current environment',
+      },
+    ],
+    security: [{ Auth: [] }],
+  }))
+  .openAPIRegistry.registerComponent('securitySchemes', 'Auth', {
+    type: 'http',
+    scheme: 'bearer',
+    bearerFormat: 'token',
+    description: 'Latitude API Key',
+  })
+
+// TODO: REMOVE
+if (process.env.NODE_ENV === 'development') {
+  const { swaggerUI } = await import('@hono/swagger-ui')
+  app.get('/ui', swaggerUI({ url: '/doc' }))
+}
 
 app.use(rateLimitMiddleware())
 app.use(authMiddleware())
